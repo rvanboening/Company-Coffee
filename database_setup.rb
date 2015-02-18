17fac1d3a0c24ed4a63c15b5e36e32eaf5687963
Rachel VanBoening <vanboeningrr@gmail.com>
2015-02-18 10:52:56 -0600

    Some changes to the DB setup

--- database_setup.rb
+++ database_setup.rb
@@ -2,10 +2,12 @@ DATABASE = SQLite3::Database.new("coffeering.db")
 
 DATABASE.results_as_hash = true
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY,
+DATABASE.execute("CREATE TABLE IF NOT EXISTS items (item_id INTEGER PRIMARY KEY,
                   drink TEXT, size TEXT, reg_decaf TEXT, cream TEXT,
                   sugar TEXT, reg_skim TEXT, whip_nowhip TEXT,
                   flavor TEXT, wet_dry TEXT)")
                   
-DATABASE.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, 
-                  user_name TEXT, office_num INTEGER, order_num INTEGER)")
+DATABASE.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, 
+                  user_name TEXT, office_num INTEGER)")
+                  
+DATABASE.execute("CREATE TABLE IF NOT EXISTS orders (item_id INTEGER, user_id INTEGER)")
