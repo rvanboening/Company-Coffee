efadfb51c09cb2c37abbaa2430bf7ad62aa6949e
Rachel VanBoening <vanboeningrr@gmail.com>
2015-02-22 10:57:19 -0600

    Barista loop is still not working. Moved gem.

--- main.rb
+++ main.rb
@@ -27,6 +27,16 @@ get "/mocha_page" do
   erb :mocha_page, :layout => :boilerplate
 end
 
+get "/americano_page" do 
+  @user_id=params["user_id"]
+  erb :americano_page, :layout => :boilerplate
+end
+
+get "/latte_page" do 
+  @user_id=params["user_id"]
+  erb :latte_page, :layout => :boilerplate
+end
+
 get "/success" do
   user_id=params["user_id"]
   @new_item=Item.new(params)
@@ -42,24 +52,9 @@ end
 get "/barista_order_page" do
   
   made_item = Item.edit(params["item_id"])
-  order= Item.print(params["item_id"])
+  Item.print(params["item_id"])
   
-  # Step 1. Have a collection of things to make labels for.
-  
-  filename = "print_orders.pdf"
-
-  # Step 2. Loop through that collection.
-
-  # The 'generate' method needs an array of information, where each item in
-  # the array has stuff that'll go on a single label.
-  
-  Prawn::Labels.generate(filename, order, user :type => "Avery5160") do |pdf, o, u|
-
-  # Step 3. For each item, create the string you want printed on each label.
-    pdf.text "Order for: #{o["item_id"]}.\n#{o["drink"]}.\n#{o["size"]}.\n#{o["reg_decaf"]}.\n#{o["cream"]}.\n
-                          #{o["sugar"]}.\n#{o["reg_skim"]}.\n#{o["whip_nowhip"]}.\n#{o["flavor"]}.\n#{o["wet_dry"]}"
-  end
-  erb :barista_order_page, :layout => :boilerplate
+ erb :barista_order_page, :layout => :boilerplate
 end
 
 
